5ff19c0de5be6b69a4bb3f9a6e41e76575cd4db7
dew <lkumar@msewa.com>
2019-04-11 19:40:33 +0530

    latest code

--- app/src/main/java/com/example/madmovegame/home/HomeActivity.java
+++ app/src/main/java/com/example/madmovegame/home/HomeActivity.java
@@ -38,6 +38,7 @@ public class HomeActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+        navigationView.setItemIconTintList(null);
 
         ImageView backImg = navigationView.getHeaderView(0)
                 .findViewById(R.id.back);
